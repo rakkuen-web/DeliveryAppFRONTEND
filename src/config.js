1b1b34bc69568e44bc578956b47f183364cefedf
rakkuen-web <62544310+rakkuen-web@users.noreply.github.com>
2025-08-26 17:37:26 +0100

    g

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-const SOCKET_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 export const API_BASE_URL = API_URL + '/api';
 export const SOCKET_BASE_URL = SOCKET_URL;
