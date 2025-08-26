d483a1f75adca6428705b05b3a031f9e5973d546
rakkuen-web <62544310+rakkuen-web@users.noreply.github.com>
2025-08-26 19:39:10 +0100

    s

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
-const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = process.env.REACT_APP_API_URL || 'https://deliveryappbackend-production.up.railway.app';
+const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || process.env.REACT_APP_API_URL || 'https://deliveryappbackend-production.up.railway.app';
 
 export const API_BASE_URL = API_URL + '/api';
 export const SOCKET_BASE_URL = SOCKET_URL;
